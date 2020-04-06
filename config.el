80bc13b9aeef70e33246079a2e9c52de9fb22b6c
Qianchuan Ye <yeqianchuan@gmail.com>
2020-04-06 13:02:07 -0400

    More aggressive completion.

--- config.el
+++ config.el
@@ -6,7 +6,7 @@
       ;; doom-inhibit-indent-detection t
       +evil-want-o/O-to-continue-comments nil
       ;; evil-want-fine-undo t
-      company-idle-delay 0.5
+      company-idle-delay 0
       display-line-numbers-type nil
       flycheck-global-modes nil
       custom-file (concat doom-private-dir "custom.el"))
@@ -30,6 +30,12 @@
       :n [tab] #'indent-for-tab-command
       :v [tab] #'yas-insert-snippet
       :g "M-;" #'comment-dwim)
+
+(map! :after company
+      :map company-active-map
+      "TAB" nil
+      [tab] nil
+      "C-SPC" #'company-complete-common-or-cycle)
 
 (load! "+textobjects")
 (load! "+latex")
