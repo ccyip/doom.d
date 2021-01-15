b983c699521ed3b9f27e35950b0a667070181c2a
Qianchuan Ye <yeqianchuan@gmail.com>
2021-01-15 00:23:15 -0500

    Add Coq key bindings.

--- +coq.el
+++ +coq.el
@@ -1,8 +1,8 @@
 ;;; -*- lexical-binding: t; -*-
 
 (setq proof-splash-seen t)
-(map! :after pg-user
-      :map proof-mode-map
+(map! :after coq-mode
+      :map coq-mode-map
       "<f2>" #'proof-undo-last-successful-command
       "<f3>" #'proof-assert-next-command-interactive
       "<f4>" #'proof-goto-point
@@ -13,15 +13,25 @@
       "[" #'+pg/hydra/proof-undo-last-successful-command
       "n" #'+pg/hydra/proof-assert-next-command-interactive
       "]" #'+pg/hydra/proof-assert-next-command-interactive
-      (:prefix ("s" . "store")
-        "b" #'proof-store-buffer-win
-        "g" #'proof-store-goals-win
-        "r" #'proof-store-response-win))
-(map! :after coq-mode
-      :map coq-mode-map
-      :localleader
       (:prefix "a"
-        "n" #'coq-LocateNotation))
+       "n" #'coq-LocateNotation
+       "s" #'coq-search
+       "h" #'coq-PrintHint
+       "p" #'coq-show-proof)
+      (:prefix ("s" . "store")
+       "b" #'proof-store-buffer-win
+       "g" #'proof-store-goals-win
+       "r" #'proof-store-response-win)
+      (:prefix ("o" . "option")
+       "A" #'coq-set-printing-all
+       "a" #'coq-unset-printing-all
+       "I" #'coq-set-printing-implicit
+       "i" #'coq-unset-printing-implicit
+       "C" #'coq-set-printing-coercions
+       "c" #'coq-unset-printing-coercions
+       "U" #'coq-set-printing-universes
+       "u" #'coq-unset-printing-universes
+       ))
 
 ;; TODO: remove later. `+company-init-backends-h' in
 ;; `after-change-major-mode-hook' overrides `company-backends' set by
