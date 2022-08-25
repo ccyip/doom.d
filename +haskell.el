4b0ffe842d73c77a36a2b89a263fddafa95b8526
Qianchuan Ye <yeqianchuan@gmail.com>
2022-08-24 20:36:42 -0400

    Tweak haskell lsp.

--- +haskell.el
+++ +haskell.el
@@ -1,4 +1,6 @@
 ;;; -*- lexical-binding: t; -*-
 
 (after! haskell-mode
-  (add-hook 'haskell-mode-hook #'rainbow-delimiters-mode))
+  (add-hook 'haskell-mode-hook #'rainbow-delimiters-mode)
+  (setq lsp-haskell-plugin-ghcide-completions-config-auto-extend-on nil
+        lsp-haskell-plugin-ghcide-completions-config-snippets-on nil))
