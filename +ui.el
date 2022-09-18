a1b04c8d7ea07aba79929be95431df2ff04423c7
Qianchuan Ye <yeqianchuan@gmail.com>
2022-09-18 16:38:01 -0400

    Restructure font loader

--- +ui.el
+++ +ui.el
@@ -2,22 +2,24 @@
 
 (add-hook 'window-setup-hook #'toggle-frame-maximized)
 
-(setq +font-source-code-pro (font-spec :family "Source Code Pro" :size 18))
-(setq +font-jetbrains-mono (font-spec :family "JetBrains Mono" :size 18))
-(setq +font-cascadia-code (font-spec :family "Cascadia Code" :size 18))
-(setq +font-victor-mono (font-spec :family "Victor Mono" :size 18))
-(setq +font-fira-code (font-spec :family "Fira Code" :size 18))
-(setq +font-comic-code (font-spec :family "Comic Code Ligatures" :size 18))
-(setq +font-stix (font-spec :family "STIX Two Math"))
+(setq +font-size 18)
 
-(setq doom-font +font-comic-code
-      doom-unicode-font +font-stix)
+(setq +font-source-code-pro "Source Code Pro")
+(setq +font-jetbrains-mono "JetBrains Mono")
+(setq +font-cascadia-code "Cascadia Code")
+(setq +font-victor-mono "Victor Mono")
+(setq +font-fira-code "Fira Code")
+(setq +font-comic-code "Comic Code Ligatures")
+(setq +font-stix "STIX Two Math")
+
+(setq doom-font (font-spec :family +font-comic-code :size +font-size)
+      doom-unicode-font (font-spec :family +font-stix))
 
 (add-hook! 'after-setting-font-hook :append
   (defun +ui-tweak-unicode-font-h ()
     (set-fontset-font t 'unicode doom-font nil 'prepend)))
 
-;; (defun +font-test (font)
-;;   (setq doom-font font)
+;; (defun +font-test (font sz)
+;;   (setq doom-font (font-spec :family font :size sz))
 ;;   (set-frame-font doom-font)
 ;;   (+ui-tweak-unicode-font-h))
